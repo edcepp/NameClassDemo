b955ead92aa13eaa918c997dad482b5b725b2032
edcepp <epp@zdome.net>
2017-01-30 19:07:35 -0800

    GUI Demo Start

--- app/src/main/java/net/zdome/cs371/epp/nameclassdemo/MainActivity.java
+++ app/src/main/java/net/zdome/cs371/epp/nameclassdemo/MainActivity.java
@@ -1,13 +1,40 @@
 package net.zdome.cs371.epp.nameclassdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.graphics.Color;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    @Override
+private TextView advise;
+private Button panicButton;
+
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        advise = (TextView)findViewById(R.id.advise);
+        panicButton  = (Button)findViewById(R.id.PanicButton);
+        panicButton.setOnClickListener(new PanicButtonListener());
+        /*
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {}
+        });
+        */
     }
+
+    private class PanicButtonListener implements View.OnClickListener{
+        public void onClick(View v)
+        {
+            advise.setTextColor(Color.RED);
+        }
+    }
+
 }
